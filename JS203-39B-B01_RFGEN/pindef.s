f462414f4a8e91b97ef130a91122f4aea17ce1c0
Kevin Liao <kevin.josn@gmail.com>
2025-06-06 18:54:44 +0800

    edit

--- JS203-39B-B01_RFGEN/pindef.s
+++ JS203-39B-B01_RFGEN/pindef.s
@@ -1,118 +1,189 @@
+;PIN 1===========================		
+.EQU    SCLK_O		,LATB
+.EQU    SCLK_IO	        ,TRISB
+.EQU    SCLK_O_P	,9
+.EQU    SCLK_IO_P	,9
+;PIN 2===========================		
+.EQU    SDA_O		,LATC
+.EQU    SDA_IO		,TRISC
+.EQU    SDA_O_P		,6
+.EQU    SDA_IO_P	,6
+;PIN 3===========================		
+.EQU    SLE_O		,LATC
+.EQU    SLE_IO		,TRISC
+.EQU    SLE_O_P		,7
+.EQU    SLE_IO_P	,7
+;PIN 4===========================		
+.EQU    CE_O	        ,LATC
+.EQU    CE_IO	        ,TRISC
+.EQU    CE_O_P	        ,8
+.EQU    CE_IO_P	        ,8
+;PIN 5===========================		
+.EQU    RFOUT_EN_O	,LATC
+.EQU    RFOUT_EN_IO	,TRISC
+.EQU    RFOUT_EN_O_P	,9
+.EQU    RFOUT_EN_IO_P	,9
+;PIN 8===========================		
+.EQU    LD_I	        ,PORTB
+.EQU    LD_IO	        ,TRISB
+.EQU    LD_I_P	        ,10
+.EQU    LD_IO_P	        ,10
+;PIN 9===========================		
+.EQU    NC9_O	        ,LATB
+.EQU    NC9_IO	        ,TRISB
+.EQU    NC9_O_P	        ,11
+.EQU    NC9_IO_P	,11
+;PIN 10==========================		
 
-;======================================================
-;=======================PIN1 
-.EQU RF_PW_I		,PORTB
-.EQU RF_PW_IO		,TRISB
-.EQU RF_PW_I_P		,0
-.EQU RF_PW_IO_P		,0
-;=======================PIN2 
-.EQU RFIN_EN_I		,PORTB
-.EQU RFIN_EN_IO	        ,TRISB
-.EQU RFIN_EN_I_P	,1
-.EQU RFIN_EN_IO_P	,1
-;=======================PIN3 	PGC
-.EQU NC3_I		,PORTB
-.EQU NC3_IO	        ,TRISB
-.EQU NC3_I_P	        ,2
-.EQU NC3_IO_P	        ,2
-;=======================PIN4 	PGD
-.EQU NC4_I		,PORTB
-.EQU NC4_IO		,TRISB
-.EQU NC4_I_P		,3
-.EQU NC4_IO_P		,3
-;=======================PIN5 	VSS
-;=======================PIN6 	OSC1
-.EQU NC6_I		,PORTA
-.EQU NC6_IO	        ,TRISA
-.EQU NC6_I_P	        ,2
-.EQU NC6_IO_P	        ,2
-;=======================PIN7	OSC2 
-.EQU NC7_I		,PORTA
-.EQU NC7_IO	        ,TRISA
-.EQU NC7_I_P	        ,3
-.EQU NC7_IO_P	        ,3
-;=======================PIN8 
-.EQU NC8_O		,LATB
-.EQU NC8_IO	        ,TRISB
-.EQU NC8_O_P	        ,4
-.EQU NC8_IO_P	        ,4
-;=======================PIN9	 
-.EQU NC9_O		,LATA
-.EQU NC9_IO	        ,TRISA
-.EQU NC9_O_P	        ,4
-.EQU NC9_IO_P	        ,4
-;=======================PIN10 	VDD
-;=======================PIN11 
-.EQU NC11_O		,LATB
-.EQU NC11_IO	        ,TRISB
-.EQU NC11_O_P	        ,5
-.EQU NC11_IO_P	        ,5
-;=======================PIN12 
-.EQU ADR0_I		,PORTB
-.EQU ADR0_IO	        ,TRISB
-.EQU ADR0_I_P	        ,6
-.EQU ADR0_IO_P	        ,6
-;=======================PIN13 
-.EQU ADR1_I		,PORTB
-.EQU ADR1_IO	        ,TRISB
-.EQU ADR1_I_P	        ,7
-.EQU ADR1_IO_P	        ,7
-;=======================PIN14 
-.EQU ADR2_I		,PORTB
-.EQU ADR2_IO	        ,TRISB
-.EQU ADR2_I_P	        ,8
-.EQU ADR2_IO_P	        ,8
-;=======================PIN15 
-.EQU NC15_O		,LATB
-.EQU NC15_IO	        ,TRISB
-.EQU NC15_O_P	        ,9
-.EQU NC15_IO_P	        ,9
-;=======================PIN16 	VSS
-;=======================PIN17	VCAP 
-;=======================PIN18 	
-.EQU OP_TX_O		,LATB
-.EQU OP_TX_IO	        ,TRISB
-.EQU OP_TX_O_P	        ,10
-.EQU OP_TX_IO_P	        ,10
-;=======================PIN19	 
-.EQU OP_RX_I		,PORTB
-.EQU OP_RX_IO	        ,TRISB
-.EQU OP_RX_I_P	        ,11
-.EQU OP_RX_IO_P	        ,11
-;=======================PIN20 	VSS
-.EQU NC20_O		,LATB
-.EQU NC20_IO	        ,TRISB
-.EQU NC20_O_P	        ,12
-.EQU NC20_IO_P	        ,12
-;=======================PIN21 
-.EQU NC21_O		,LATB
-.EQU NC21_IO	        ,TRISB
-.EQU NC21_O_P	        ,13
-.EQU NC21_IO_P	        ,13
-;=======================PIN22 
-.EQU NC22_O		,LATB
-.EQU NC22_IO	        ,TRISB
-.EQU NC22_O_P	        ,14
-.EQU NC22_IO_P	        ,14
-;=======================PIN23 
-.EQU NC23_O		,LATB
-.EQU NC23_IO	        ,TRISB
-.EQU NC23_O_P	        ,15
-.EQU NC23_IO_P	        ,15
-.EQU TESTP_O		,LATB
-.EQU TESTP_O_P	        ,15
+.EQU    MCU_RX_I	,PORTB
+.EQU    MCU_RX_IO	,TRISB
+.EQU    MCU_RX_I_P	,12
+.EQU    MCU_RX_IO_P	,12
 
 
-;=======================PIN24 	VSS
-;=======================PIN25	VDD 
-;=======================PIN26 	MCLR
-;=======================PIN27 
-.EQU NC27_O		,LATA
-.EQU NC27_IO	        ,TRISA
-.EQU NC27_O_P	        ,0
-.EQU NC27_IO_P	        ,0
-;=======================PIN28 
-.EQU PW_RST_O		,LATA
-.EQU PW_RST_IO	        ,TRISA
-.EQU PW_RST_O_P	        ,1
-.EQU PW_RST_IO_P	,1
+;PIN 11==========================		
+
+.EQU    MCU_TX_O	,LATB
+.EQU    MCU_TX_IO	,TRISB
+.EQU    MCU_TX_O_P	,13
+.EQU    MCU_TX_IO_P	,13
+
+
+;PIN 12==========================		
+.EQU    NC12_O		,LATA
+.EQU    NC12_IO	        ,TRISA
+.EQU    NC12_O_P	,10
+.EQU    NC12_IO_P	,10
+;PIN 13==========================		
+.EQU    NC13_O		,LATA
+.EQU    NC13_IO	        ,TRISA
+.EQU    NC13_O_P	,7
+.EQU    NC13_IO_P	,7
+;PIN 14==========================		
+.EQU    NC14_O	        ,LATB
+.EQU    NC14_IO	        ,TRISB
+.EQU    NC14_O_P	,14
+.EQU    NC14_IO_P	,14
+;PIN 15==========================		
+.EQU    NC15_O		,LATB
+.EQU    NC15_IO	        ,TRISB
+.EQU    NC15_O_P	,15
+.EQU    NC15_IO_P	,15
+;PIN 19==========================		
+.EQU    NC19_O		,LATA
+.EQU    NC19_IO	        ,TRISA
+.EQU    NC19_O_P	,0
+.EQU    NC19_IO_P	,0
+;PIN 20==========================		
+.EQU    NC20_O		,LATA
+.EQU    NC20_IO		,TRISA
+.EQU    NC20_O_P	,1
+.EQU    NC20_IO_P	,1
+;PIN 21==========================		
+.EQU    PGD_O		,LATB
+.EQU    PGD_I		,PORTB
+.EQU    PGD_IO		,TRISB
+.EQU    PGD_O_P		,0
+.EQU    PGD_I_P		,0
+.EQU    PGD_IO_P	,0
+;PIN 22==========================		
+.EQU    PGC_I		,PORTB
+.EQU    PGC_IO		,TRISB
+.EQU    PGC_I_P		,1
+.EQU    PGC_IO_P	,1
+;PIN 23==========================		
+.EQU    LED_O		,LATB
+.EQU    LED_IO		,TRISB
+.EQU    LED_O_P		,2
+.EQU    LED_IO_P	,2
+;PIN 24==========================		
+;.EQU    MCU_RX_I	,PORTB
+;.EQU    MCU_RX_IO	,TRISB
+;.EQU    MCU_RX_I_P	,3
+;.EQU    MCU_RX_IO_P	,3
+;PIN 25==========================		
+;.EQU    MCU_TX_O	,LATC
+;.EQU    MCU_TX_IO	,TRISC
+;.EQU    MCU_TX_O_P	,0
+;.EQU    MCU_TX_IO_P	,0
+;PIN 26==========================		
+.EQU    NC26_O		,LATC
+.EQU    NC26_IO		,TRISC
+.EQU    NC26_O_P	,1
+.EQU    NC26_IO_P	,1
+;PIN 27==========================		
+.EQU    NC27_O		,LATC
+.EQU    NC27_IO		,TRISC
+.EQU    NC27_O_P	,2
+.EQU    NC27_IO_P	,2
+;PIN 30==========================		
+.EQU    OSCI_I	        ,PORTA
+.EQU    OSCI_IO	        ,TRISA
+.EQU    OSCI_I_P	,2
+.EQU    OSCI_IO_P	,2
+;PIN 31==========================		
+.EQU    OSCO_O	        ,LATA
+.EQU    OSCO_IO	        ,TRISA
+.EQU    OSCO_O_P	,3
+.EQU    OSCO_IO_P	,3
+;PIN 32==========================		
+.EQU    NC32_O		,LATA
+.EQU    NC32_IO		,TRISA
+.EQU    NC32_O_P	,8
+.EQU    NC32_IO_P	,8
+;PIN 33==========================		
+.EQU    NC33_O		,LATB
+.EQU    NC33_IO		,TRISB
+.EQU    NC33_O_P	,4
+.EQU    NC33_IO_P	,4
+;PIN 34==========================		
+.EQU    NC34_O		,LATA
+.EQU    NC34_IO		,TRISA
+.EQU    NC34_O_P	,4
+.EQU    NC34_IO_P	,4
+;PIN 35==========================		
+.EQU    NC35_O		,LATA
+.EQU    NC35_IO		,TRISA
+.EQU    NC35_O_P	,9
+.EQU    NC35_IO_P	,9
+;PIN 36==========================		
+.EQU    NC36_O		,LATC
+.EQU    NC36_IO		,TRISC
+.EQU    NC36_O_P	,3
+.EQU    NC36_IO_P	,3
+;PIN 36==========================		
+.EQU    NC37_O		,LATC
+.EQU    NC37_IO		,TRISC
+.EQU    NC37_O_P	,4
+.EQU    NC37_IO_P	,4
+;PIN 36==========================		
+.EQU    NC38_O		,LATC
+.EQU    NC38_IO		,TRISC
+.EQU    NC38_O_P	,5
+.EQU    NC38_IO_P	,5
+;PIN 41==========================		
+.EQU    NC41_O		,LATB
+.EQU    NC41_IO		,TRISB
+.EQU    NC41_O_P	,5
+.EQU    NC41_IO_P	,5
+;PIN 42==========================		
+.EQU    NC42_O		,LATB
+.EQU    NC42_IO		,TRISB
+.EQU    NC42_O_P	,6
+.EQU    NC42_IO_P	,6
+;PIN 43==========================		
+.EQU    NC43_O		,LATB
+.EQU    NC43_IO		,TRISB
+.EQU    NC43_O_P	,7
+.EQU    NC43_IO_P	,7
+;PIN 44==========================		
+.EQU    SDO_I	        ,PORTB
+.EQU    SDO_IO	        ,TRISB
+.EQU    SDO_I_P	        ,8
+.EQU    SDO_IO_P	,8
+
+
+
+
+
+
